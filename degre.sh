ea7db53cd667cd34b953247525eae08ebe7d331d
Lamia123456789 <95941572+Lamia123456789@users.noreply.github.com>
2021-12-30 00:42:28 +0200

    Add files via upload

---
+++ degre.sh
@@ -0,0 +1,4 @@
+echo "enter degree"
+read c
+Faren=$(expr $c \* 9 / 5 + 32)
+echo "$Faren"
---
+++ digits.sh
@@ -0,0 +1,11 @@
+echo "enter num"
+read num
+while [ $num -gt 0 ]
+do
+n=$(expr $num / 10)
+mod= $(expr $num - $n \* 10)
+num= $(expr $num / 10)
+sum= $(expr $mod + $num)
+echo $sum
+done
+
